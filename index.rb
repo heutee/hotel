eb49cec60d5459e227a4c1efc6c450b7345c0339
heutee <hannah_332004@yahoo.com>
2014-05-03 01:12:18 +0800

    updated 1:12

--- index.rb
+++ index.rb
@@ -16,18 +16,14 @@ class Room
 	attr_writer :room
 	attr_reader :room
 	
-	def initializate(first_name, last_name, contact_num, email, date, days, room)
+	def initialize(first_name, last_name, contact_num, email, date, days, room_type)
 		@first_name = first_name
 		@last_name = last_name
 		@contact_num = contact_num
 		@email = email
 		@date = date
 		@days = days
-		@room = room
-	end
-
-	def occupado
-		"Customer: #{@first_name} #{last_name} &mdash; You reserved a #{@room} room for #{@days} days starting on #{@date}. We will contact you at your given contact information (#{@contact_num}, #{@email}) should anything come up. Thank you for choosing to stay in Hotel California."
+		@room_type = room_type
 	end
 end
 
@@ -47,7 +43,7 @@ get '/form' do
 	erb :form
 end
 
-post '/form' do
-	@room = Room.new params[:first_name, :last_name, :contact_num, :email, :date, :days, :room]
+post '/form_results' do
+	@room = Room.new(params[:first_name], params[:last_name], params[:contact_num], params[:email], params[:date], params[:days], params[:room])
 	erb :form_results
 end
